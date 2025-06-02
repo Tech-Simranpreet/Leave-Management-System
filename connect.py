434ceb991adacaa152442c43d1ae3332359b56e1
Simranpreet-Kaur-tech <119108495+Simranpreet-Kaur-tech@users.noreply.github.com>
2025-06-02 22:46:35 +1200

    update connection

--- connect.py
+++ connect.py
@@ -1,7 +1,7 @@
 # These details are available on the first MySQL Workbench screen
 # Usually called 'Local Instance'
-dbuser = "root" #PUT YOUR MySQL username here - usually root
-dbpass = "dir123" #PUT YOUR PASSWORD HERE
-dbhost = "localhost" 
-dbport = "3306"
-dbname = "leave_management_system"
+dbuser = "root"  # PUT YOUR MySQL username here - usually root
+dbpass = "MVBGOoInGREzUYfOYiiJMxATeZwRnGfW"  # PUT YOUR PASSWORD HERE
+dbhost = "switchback.proxy.rlwy.net"
+dbport = "45416"
+dbname = "railway"
